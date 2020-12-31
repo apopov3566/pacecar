26a2c346d94dddaa23ab6826e62333c1258b338c
Default <default@pop-os.localdomain>
2020-12-30 20:53:16 -0500

    Add basic controller
    Communication between controller and computer not yet implemented

---
+++ controller/modes.h
@@ -0,0 +1,12 @@
+enum modes{PASSTHROUGH, COMPUTER, LOST};
+
+const double WIDTH = 500;
+
+const int STEER_CENTER = 92;
+const double STEER_WIDTH = 45;
+double steer_prop = 1.0;
+
+const int THROTTLE_CENTER = 90;
+const double THROTTLE_WIDTH = 45;
+
+const int mode_colors[3][3] = {{100,0,0},{0,100,0},{0,0,100}};
